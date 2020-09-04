97324ec510025ef9a19102a42c5d88d488348fb6
Ethan-boy <55939262+Ethan-boy@users.noreply.github.com>
2020-09-04 16:50:46 +0800

    刘诗祥第一次作业

---
+++ 001.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+#include <iostream>
+#include <string.h>
+class Cat{
+	private:
+		int age;
+		int weight;
+		char color;
+	public:
+		void SetAge(int a);
+		void SetWeight(int b);
+		void SetColor(char c);
+};
+void Cat::SetAge(inta)
+{
+	this->age=a;
+}
+void Cat::SetWeight(int b)
+{
+	this->weight=b;
+}
+void Cat::SetColor(char c)
+{
+	this->color=c;
+}              
