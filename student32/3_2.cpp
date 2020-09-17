5012c5463ba85132a52e05ae9ac8ccb1db413716
hardcompiler <55878632+hardcompiler@users.noreply.github.com>
2020-09-17 11:31:28 +0800

    沈祺尧第三周第二次作业

---
+++ student32/3_2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	if(argc<3)
+	{
+		return 0;
+	}
+	fstream infile,outfile;
+	infile.open(argv[1],ios::in|ios::binary);
+	outfile.open(argv[2],ios::out|ios::binary);
+	if(!infile)
+	{
+		cout<<"不能成功打开输入文件："<<argv[1]<<'\n';
+		return 0;
+	}
+	if(!outfile)
+	{
+		cout<<"不能成功打开目的文件："<<argv[2]<<'\n';
+		 return 0;
+	}
+	char buff[4096];
+	int n;
+	while(!infile.eof()){
+		infile.read(buff,4096);
+		n=infile.gcount();
+		outfile.write(buff,n);
+	}
+	infile.close();
+	outfile.close();
+	return 0;
+} 
+
